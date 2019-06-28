5746f348cd63162bc636adf8dcdf0235f014cdee
=aortizg <=aortiz@metaenlace.com>
2019-06-28 12:42:32 +0200

    v1.0 - Remove comments

--- open-links.js
+++ open-links.js
@@ -1,15 +1,9 @@
-// var x = document.getElementsByClassName("title may-blank loggedin");
-// for(i = 0; i < x.length; i++){
-	// window.open(x[i].href, '_blank')	
-// }
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
 	var x = document.getElementsByClassName("title may-blank loggedin");
 	var response = []
 	for(i = 0; i < x.length; i++){
 		url = x[i].href
 		response.push(url);
-		// chrome.tabs.create({url: url, selected: false});
-		//window.open(x[i].href, '_blank')	
 	}
 	sendResponse(response);
 });
